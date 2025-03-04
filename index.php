0c17af379f74d9a2015b087fd93cff12ee77b4a8
Antoine Haro <antoine.haro@limayrac.fr>
2025-03-04 14:37:09 +0100

    ajout page accueil (voyage entre les pages)

--- index.php
+++ index.php
@@ -4,10 +4,18 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Accueil FAQ</title>
-    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="templatHTML/css/style.css">
 </head>
 <body>
+    <h1>Bienvenue sur la page d'accueil</h1>
+    <br>
+    <a href="templateHTML/login.php">Se connecter</a>
+    <br>
+    <a href="templateHTML/register.php">S'inscrire</a>
+    <br>
+    <a href="index.php">Se déconecter</a>
 
+    <h3>Présentation de l'App Faqq</h3>
     
 </body>
 </html>
