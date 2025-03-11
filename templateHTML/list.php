7805249ff04d7d4536b38cabf98c10492506277e
Antoine Haro <antoine.haro@limayrac.fr>
2025-03-11 14:40:04 +0100

    ajout list.php

---
+++ templateHTML/list.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/style.css">
+    <title>inscription</title>
+</head>
+<body>
+
+    <header>
+        <div class="header-container">
+            <button onclick="window.location.href='ajouter_question.html'">Ajouter une question</button>
+            <input type="text" placeholder="Rechercher une question">
+            <button onclick="window.location.href='index.php'">Déconnexion</button>
+        </div>
+    </header>
+
+    <div class="container">
+    <div class="button.list">
+        <h2>Liste des questions</h2>
+
+        <table>
+                <tr>
+                    <th>Ligue</th>
+                    <th>User/Pseudo</th>
+                    <th>Question</th>
+                </tr>
+
+                <tr>
+                    <td>...</td>
+                    <td>...</td>
+                    <td>...</td>
+                </tr>
+
+                <tr>
+                    <td>...</td>
+                    <td>...</td>
+                    <td>...</td>
+                </tr>
+
+                <tr>
+                    <td>...</td>
+                    <td>...</td>
+                    <td>...</td>
+                </tr>
+
+                <tr>
+                    <td>...</td>
+                    <td>...</td>
+                    <td>...</td>
+                    <td>...</td>
+                </tr>
+        </table>
+         
+    
+    </div>
+</body>
+</html>
