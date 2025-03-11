28dbc2486f76ed114d033113070e0905a1eb1fba
Antoine Haro <antoine.haro@limayrac.fr>
2025-03-11 14:03:43 +0100

    ajout page add.php

---
+++ add.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Ajouter une Question</title>
+</head>
+<body>
+
+    <header id="list-header">
+        <div class="header-container">
+            <input type="text" placeholder="Rechercher une question">
+        </div>
+    </header>
+
+    <div id="main-container" style="background-color: white; padding: 20px; border-radius: 10px; max-width: 600px; margin: auto; box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);">
+        <div id="faq-container">
+            <h2>Ajouter une Question à la FAQ</h2>
+            <form>
+                <label for="question">Question :</label>
+                <input type="text" id="question" name="question" required>
+                    <input type="submit" value="Ajouter">
+            </form>
+        </div>
+</body>
+</html>
