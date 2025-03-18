9bef244fe527c3960c4a57b974897ac666c5ab60
tesnim16 <tesnim.benama@limayrac.fr>
2025-03-18 11:46:33 +0100

    Correction list.php, list.css, style.css

--- templateHTML/list.php
+++ templateHTML/list.php
@@ -3,14 +3,14 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/list.css">
     <title>inscription</title>
 </head>
 <body>
 
     <header>
         <div class="header-container">
-            <button onclick="window.location.href='ajouter_question.html'">Ajouter une question</button>
+            <button onclick="window.location.href='add.php'">Ajouter une question</button>
             <input type="text" placeholder="Rechercher une question">
             <button onclick="window.location.href='index.php'">Déconnexion</button>
         </div>
